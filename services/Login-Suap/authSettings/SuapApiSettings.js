f35d64c8ed6fbe1407a84e7514743c8be410f913
aldcejam <aldcejamj923@gmail.com>
2023-03-01 14:15:15 -0300

    exemplo

--- services/Login-Suap/authSettings/SuapApiSettings.js
+++ services/Login-Suap/authSettings/SuapApiSettings.js
@@ -1,9 +1,16 @@
 const SUAP_URL = "https://suap.ifrn.edu.br";
 
 export const SuapApiSettings = {
-    CLIENT_ID: "hdBUL9RxxrkwppEk53TMtX5hiXdUqHJQTLRJQjId",
-    REDIRECT_URL: "http://127.0.0.1:5500/index.html",
+    CLIENT_ID: "COLOQUE AQUI O SEU CLIENT_ID",
+    REDIRECT_URL: "URL DE REDIRECIONAMENTO (CONFIGURADA NO SUAP))",
     AUTH_HOST: SUAP_URL,
     SCOPE: "identificacao email documentos_pessoais",
     URL_FOR_GET_USERDATA: `${SUAP_URL}/api/v2/minhas-informacoes/meus-dados/`,
 };
+
+/* Tanto o CLIENT_ID quanto REDIRECT_URL podem ser configurados em: 
+https://suap.ifrn.edu.br/api/ */
+
+/* URL_FOR_GET_USERDATA: parte da url pode ser acessada em 
+https://suap.ifrn.edu.br/api/docs/ . Nela podera encontrar quais 
+dados podem ser consumidos */
